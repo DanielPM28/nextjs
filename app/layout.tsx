6bff8ef3f667ae317cdb5348d367e56516728ac7
DanielPM28 <153576338+DanielPM28@users.noreply.github.com>
2024-03-11 15:53:13 -0500

    Primeros pasos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+>>>>>>> 745b799 (Primeros pasos)
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+<<<<<<< HEAD
       <body>{children}</body>
+=======
+      <body className={`${inter.className} antialiased`}>{children}</body>
+>>>>>>> 745b799 (Primeros pasos)
     </html>
   );
 }
